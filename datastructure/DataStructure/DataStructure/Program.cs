9e08a4417681a55985b7af637b3b1f6de4d39cc1
Abhishe <abhisheknarwariya813@gmail.com>
2022-04-04 14:23:44 +0530

    Added Day17

---
+++ datastructure/DataStructure/DataStructure/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            try
+            {
+                Console.WriteLine("1. Unorderd array \n2.Ordered array");
+                Console.WriteLine("enter your option:");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UnorderedList list = new UnorderedList();
+                        list.Unorderd();
+                        break;
+
+                    case 2:
+                        OrderedList check = new OrderedList();
+                        check.Ordered();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("invalid input!!");
+                Console.WriteLine(e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ datastructure/QUEUE/QUEUE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Queue
+{  
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            CashCounter checkOne = new CashCounter();
+            checkOne.Queue();
+           
+        }
+    }
+}
